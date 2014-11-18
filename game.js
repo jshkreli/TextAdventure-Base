92196b45ea1acc168029c02144cd27314f4b0a3e
jshkreli <Jacqueline.Shkreli1@marist.edu>
2014-11-18 09:48:15 -0500

    Lab 10 game.js

--- game.js
+++ game.js
@@ -16,17 +16,36 @@ var player = function(){
 var interpret = function(input){
     var str = input.toLowerCase().trim();
     var res = str.split(" ");
-    var a = splitInput(' ');
-    splitInput.remove(0);
-    var inventory ='';
-    for(i = 1; i < splitInput.length; i++0{
-      inventory = inventory + splitInput[i] + ' ';
+    //var a = splitInput(' ');
+    //splitInput.remove(0);
+    var inventory = [];
+    for(i = 1; i < res.length; i++){
+      inventory.push(res[i] + ' ');
     };
     return inventory;
 };
 
 var execute = function(inventory){
-    var action = inventory[0];
-    var object = inventory[1];
-    player[action](object);
+    var action = inventory.splice(0,0);
+    var object = inventory;
+    //player[action](object);
+    var action = function(object){
+    };
 };  
+
+var report = function(inventory){
+    var list = document.getElementById('inventoryList');
+    for (var i = 0; i < inventory.length; i++){
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(inventory[i]));
+      list.appendChild(li);
+    };
+};
+
+var gameStep = function(){
+    if(event.keyCode === 13){
+      var array = interpret(action.value);
+      excute(array);
+      report(array);
+   };
+};
